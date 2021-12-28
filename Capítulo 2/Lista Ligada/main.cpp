df466fdd8ad596814c2c9df45cb0e16fd1819db0
AlexM <alex.mariacca@outlook.com>
2021-12-28 16:02:21 -0600

    Eliminé el test con nodo

--- Capítulo 2/Lista Ligada/main.cpp
+++ Capítulo 2/Lista Ligada/main.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
-#include "Nodo.h"
+#include "ListaLigada.h"
+
 
 using namespace std;
 
 int main(){
 
-    Nodo<int> *nodo1 = new Nodo<int>;
-    Nodo<int> *nodo2 = new Nodo<int>;
-    
-    nodo1->valor = 3;
-    nodo2->valor = 4;
 
-    nodo1->siguiente = nodo2;
-
-    nodo1->imprimir(nodo1);
-
+    ListaLigada<int> l1; 
 
     return 0;
 }
