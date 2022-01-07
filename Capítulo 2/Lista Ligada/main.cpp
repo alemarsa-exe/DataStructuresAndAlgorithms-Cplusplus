61c902254caa717ccbfc0720805741b087e90e81
AlexM <alex.mariacca@outlook.com>
2022-01-07 00:22:04 -0600

    Hice más bonito el main.cpp

--- Capítulo 2/Lista Ligada/main.cpp
+++ Capítulo 2/Lista Ligada/main.cpp
@@ -7,34 +7,51 @@ using namespace std;
 
 int main(){
 
-    cout << "Con enteros" << endl;
-    ListaLigada<int> l1; 
+    cout << "\nHola, prueba de lista ligada con enteros" << endl;
+    
+    ListaLigada<int> l1;
+
+    //Imprimir la lista vacía
     l1.imprimir();
-    l1.buscar(0);
-    l1.insertarHead(22);
-    l1.insertarTail(44);
-    l1.insertarPorIndex(0, 1);
-    l1.insertarPorIndex(3, 4);
-    l1.insertarPorIndex(1, 69);
-    l1.buscar(22);
-    l1.buscar(0);
+
+    //Probar métodos de inserción
+    l1.insertarHead(8);
+    l1.insertarTail(900);
+    l1.insertarPorIndex(1, 38);
+    l1.insertarHead(-12);
+    l1.insertarTail(999);
+    l1.insertarPorIndex(5, 923764);
+
+    //Reimprimir para corroborar que funciona
     l1.imprimir();
+
+    //Probar métodos de búsqueda y obtención
+    l1.obtener(1);
+    l1.buscar(8);
+    l1.buscar(200);
+
+    //Probar métodos de borrado
     l1.borrarHead();
     l1.borrarTail();
-    l1.insertarTail(90);
-    l1.borrarPorIndex(2);
-    l1.imprimir();
-    
+    l1.borrarPorIndex(4);
 
-    //Ya funcionan los string :D
+    //Reimprimir para corroborar que funciona
+    l1.imprimir();
+
+
+    //Probando con strings
     
-    cout << "\nAhora con strings" << endl;
+    cout << "Ahora con strings" << endl;
+    
     ListaLigada<string> l2;
+
     l2.insertarHead("Hola");
     l2.insertarTail("estás?");
     l2.insertarPorIndex(1, "¿cómo ");
     l2.buscar("hola");
     l2.imprimir();
+    
+    cout << endl;
 
     return 0;
 }
